26649af599a1cd45d879a9b38c3b2a03ad2e0772
Victor Ferreira <vitfera@hotmail.com>
2025-02-28 16:05:42 -0300

    configuração geoDivisions

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-    'app.siteName' => 'Mapas Culturais Base Project',
+    'app.siteName' => 'Mapa Cultural do Maranhão',
     'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
@@ -9,5 +9,15 @@ return [
 
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
+
+    'app.geoDivisionsHierarchy' => [
+        'pais'              => ['name' => i::__('País'),            'showLayer' => true],
+        'estado'            => ['name' => i::__('Estado'),          'showLayer' => true],
+        'mesorregiao'       => ['name' => i::__('Mesorregião'),     'showLayer' => true],
+        // 'RI'  => ['name' => i::__('Região de integração'),'showLayer' => true],
+        'microrregiao'      => ['name' => i::__('Microrregião'),    'showLayer' => true],
+        'municipio'         => ['name' => i::__('Município'),       'showLayer' => true],
+    ],
+    'app.geoDivisionsFilters' => [21],
 
 ];
